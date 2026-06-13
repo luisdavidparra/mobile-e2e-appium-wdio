31dc91c2ad1d49f5322c24819baf5ad797f1c252
Luis David Parra <luisdavidparrah@gmail.com>
2026-06-13 23:59:35 +0200

    feat(auth): add TC-003 valid logout

--- test/pageObjects/CommonPage.js
+++ test/pageObjects/CommonPage.js
@@ -7,9 +7,31 @@ class CommonPage {
     return $("~menu item log in");
   }
 
+  get logoutSidebarButton() {
+    return $("~menu item log out");
+  }
+
+  get confirmLogoutButton() {
+    return $('android=new UiSelector().resourceId("android:id/button1")');
+  }
+
+  get logoutAlertSuccessMessage() {
+    return $('android=new UiSelector().resourceId("android:id/alertTitle")');
+  }
+
+  get logoutAlertAcceptButton() {
+    return $('android=new UiSelector().resourceId("android:id/button1")');
+  }
+
   async navigateToLoginPage() {
     await this.sidebarToggler.click();
     await this.loginSidebarButton.click();
+  }
+
+  async logout() {
+    await this.sidebarToggler.click();
+    await this.logoutSidebarButton.click();
+    await this.confirmLogoutButton.click();
   }
 }
 
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -17,4 +17,16 @@ describe("US-01 - Authentication", () => {
 
     await expect(LoginPage.invalidCredentialsMessage).toBeDisplayed();
   });
+
+  it("TC-003 - should logout successfully", async () => {
+    await CommonPage.navigateToLoginPage();
+    await LoginPage.login(standard.username, standard.password);
+
+    await CommonPage.logout();
+
+    await expect(CommonPage.logoutAlertSuccessMessage).toBeDisplayed();
+    await CommonPage.logoutAlertAcceptButton.click();
+
+    await expect(LoginPage.usernameInput).toBeDisplayed();
+  });
 });
